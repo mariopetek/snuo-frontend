9d7b10846d9e4e08efe6e510aa0fa12f3d9234cc
Mario Petek <mariopetek0@gmail.com>
2024-05-23 21:20:04 +0200

    Add icon to home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import { getRestaurants } from '@/api/restaurants'
 import { Metadata } from 'next'
 import Link from 'next/link'
+import Separator from '@/components/Separator/Separator'
+import { GiForkKnifeSpoon } from 'react-icons/gi'
 
 import styles from './page.module.css'
-import Separator from '@/components/Separator/Separator'
 
 export const metadata: Metadata = {
     title: 'SNUO | Ugostiteljski objekti'
@@ -18,9 +19,14 @@ export default async function HomePage() {
                 <h1 className={styles.mainHeading}>
                     Sustav za naručivanje u ugostiteljskim objektima
                 </h1>
+                <div className={styles.foodIconContainer}>
+                    <GiForkKnifeSpoon className={styles.foodIcon} />
+                </div>
+
                 <h2 className={styles.secondaryHeading}>
                     Odaberite ugostiteljski objekt
                 </h2>
+
                 {restaurants.map(restaurant => (
                     <Link
                         href={`/${restaurant.id_objekt}`}
